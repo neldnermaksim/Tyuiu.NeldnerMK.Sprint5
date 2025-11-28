46f606eb20a3d739c183097d327d553e830bf64f
neldnermaksim <neldnermaksim8@gmail.com>
2025-11-28 22:32:09 +0500

    Task1

---
+++ Tyuiu.NeldnerMK.Sprint5.Task1.V2.Lib/DataService.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+
+namespace Tyuiu.NeldnerMK.Sprint5.Task1.V2.Lib
+{
+    public class DataService : ISprint5Task1V2
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+
+
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+
+            double y;
+            string strv;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double result = 2 * x + 3 * Math.Pow(x, 2) + 4;
+                if (double.IsInfinity(result) || double.IsNaN(result))
+                {
+                    result = 0;
+                }
+                y = Math.Round(result, 2);
+                strv = Convert.ToString(y);
+
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strv + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strv);
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.NeldnerMK.Sprint5.Task1.V2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NeldnerMK.Sprint5.Task1.V2.Lib;
+
+
+namespace Tyuiu.NeldnerMK.Sprint5.Task1.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт №1 | Выполнил: Нельднер М. К . | СМАРТб-25-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт  #5                                                               *");
+            Console.WriteLine("* Задание #1                                                               *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Нельднер Максим Константинович | СМАРТб-25-1                   *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу ");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ: ");
+            Console.WriteLine();
+
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл"+res);
+            Console.WriteLine("Создан");
+        }
+    }
+}
